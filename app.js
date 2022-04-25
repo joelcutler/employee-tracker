1c8ff7e161af734354046a930ba95a8209405891
Joel C <joel7921111@yahoo.com>
2022-04-24 23:13:43 -0600

    large commit

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ const addToRoles = async () => {
     db.query(`SELECT dept_name FROM departments`, function (err, result) {
       if (err) throw err;
       deptartmentNames = result.map(({ dept_name }) => dept_name);
-      console.log(deptartmentNames);
+      // console.log(deptartmentNames);
       inquirer
         .prompt([
           {
@@ -114,34 +114,208 @@ const addToRoles = async () => {
         ])
         .then(({ roleName, roleSalary, roleDept }) => {
           let deptartmentId;
-          db.connect(function (err) {
-            if (err) throw err;
-            db.query(
-              `SELECT id FROM departments WHERE dept_name = "${roleDept}"`,
-              function (err, result) {
-                if (err) throw err;
-                deptartmentId = result[0].id;
-                console.log("string", deptartmentId);
-              }
-            );
-          });
-          db.connect(function (err) {
-            if (err) throw err;
-            db.query(
-              `INSERT INTO roles (title, salary, dept_id)
-            VALUES (?,?,?)`,
-              (params = [roleName, roleSalary, deptartmentId]),
-              function (err, result) {
-                if (err) throw err;
-                console.log("New role", roleName, "added successfully");
-                startMenu();
-              }
-            );
-          });
+          // db.connect(function (err) {
+          //   if (err) throw err;
+          db.query(
+            `SELECT id FROM departments WHERE dept_name = "${roleDept}"`,
+            function (err, result) {
+              if (err) throw err;
+              deptartmentId = result[0].id;
+              console.log("string", deptartmentId);
+
+              // db.connect(function (err) {
+              // if (err) throw err;
+              db.query(
+                `INSERT INTO roles (title, salary, dept_id)
+                  VALUES (?,?,?)`,
+                (params = [roleName, roleSalary, deptartmentId]),
+                function (err, result) {
+                  if (err) throw err;
+                  console.log("New role", roleName, "added successfully");
+                  startMenu();
+                }
+              );
+            }
+          );
         });
     });
   });
 };
 
+const addToEmployees = async () => {
+  let roleNames = [];
+  let managerNames = [];
+  db.connect(function (err) {
+    if (err) throw err;
+
+    db.query(`SELECT title FROM roles`, function (err, result) {
+      if (err) throw err;
+      roleNames = result.map(({ title }) => title);
+
+      db.query(
+        `SELECT first_name, last_name FROM employees`,
+        function (err, result) {
+          if (err) throw err;
+          managerNames = result.map(
+            ({ first_name, last_name }) => first_name + " " + last_name
+          );
+
+          // console.log(deptartmentNames);
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "empFirstName",
+                message: "What is the first name of the new employee?",
+              },
+              {
+                type: "input",
+                name: "empLastName",
+                message: "What is the last name of the new employee?",
+              },
+              {
+                type: "list",
+                name: "empRole",
+                message: "Which role does the new employee have?",
+                choices: roleNames,
+              },
+              {
+                type: "list",
+                name: "empManager",
+                message: "Who is the manager of the new employee?",
+                choices: managerNames,
+              },
+            ])
+            .then(({ empFirstName, empLastName, empRole, empManager }) => {
+              let roleId;
+              let managerId;
+              // db.connect(function (err) {
+              //   if (err) throw err;
+              db.query(
+                `SELECT id FROM roles WHERE title = "${empRole}"`,
+                function (err, result) {
+                  if (err) throw err;
+                  roleId = result[0].id;
+                  console.log("employee id:", roleId);
+
+                  // empManager = "Jane Doe"
+
+                  const empMangFirstName = empManager.split(" ")[0];
+                  const empMangLastName = empManager.split(" ")[1];
+                  db.query(
+                    `SELECT id FROM employees WHERE first_name = "${empMangFirstName}" AND last_name ="${empMangLastName}"`,
+                    function (err, result) {
+                      if (err) throw err;
+                      managerId = result[0].id;
+                      console.log("manager id:", managerId);
+
+                      // db.connect(function (err) {
+                      // if (err) throw err;
+                      db.query(
+                        `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+                  VALUES (?,?,?,?)`,
+                        (params = [
+                          empFirstName,
+                          empLastName,
+                          roleId,
+                          managerId,
+                        ]),
+                        function (err, result) {
+                          if (err) throw err;
+                          console.log(
+                            "New employee",
+                            empFirstName,
+                            empLastName,
+                            "added successfully"
+                          );
+                          startMenu();
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            });
+        }
+      );
+    });
+  });
+};
+
+const updateRole = async () => {
+  let empNames = [];
+  let empRoles = [];
+  db.connect(function (err) {
+    if (err) throw err;
+
+    db.query(
+      `SELECT first_name, last_name FROM employees`,
+      function (err, result) {
+        if (err) throw err;
+        empNames = result.map(
+          ({ first_name, last_name }) => first_name + " " + last_name
+        );
+        db.query(`SELECT title FROM roles`, 
+        function (err, result) {
+          if (err) throw err;
+          empRoles = result.map(({ title }) => title);
+          // console.log(empRoles, "employee roles")
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "empChoice",
+                message: "Which employee would you like to update the role of?",
+                choices: empNames,
+              },
+              {
+                type: "list",
+                name: "roleChoice",
+                message: "What's the employee's updated role?",
+                choices: empRoles,
+              }
+            ])
+            .then(({ empChoice, roleChoice }) => {
+              let empId;
+              let roleId;
+              // console.log(roleChoice,"<dem roles", empChoice, "<employee names" )
+
+              const empFirstName = empChoice.split(" ")[0];
+              const empLastName = empChoice.split(" ")[1];
+
+              db.query(
+                `SELECT id FROM employees WHERE first_name = "${empFirstName}" AND last_name ="${empLastName}"`,
+                function (err, result) {
+                  empId = result[0].id;
+                  db.query(
+                    `SELECT id FROM roles WHERE title = "${roleChoice}"`,
+                    function (err, result) {
+                      roleId = result[0].id;
+                      db.query(
+                        `UPDATE employees
+                              SET role_id = ${roleId}
+                              WHERE id = ${empId}`,
+                        function (err, result) {
+                          if (err) throw err;
+                          console.log(
+                            "Employee",
+                            empFirstName,
+                            empLastName,
+                            "updated successfully"
+                          );
+                          startMenu();
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            });
+        });
+      }
+    );
+  });
+};
 startMenu();
+
 // -options on load: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
